76e8b416f1af1262bceadfa44e674d783ce8aadc
renanvilabela <vilabelarenan@gmail.com>
2025-05-27 00:35:20 -0300

    fazendo seleção de candidatos

--- ValidationSystem/src/candidatura/ProcessoSeletivo.java
+++ ValidationSystem/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,33 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(200.0);
+		selecaoCandidatos();
+		//analisarCandidato(1900.0);
+		//analisarCandidato(2200.0);
+		//analisarCandidato(200.0);
+	}
+	static void selecaoCandidatos() {
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELLA", "DANIELA", "JORGE"};
+	
+		int candidatosSelecionados = 0;
+		int candidatosAtual=0;
+		double salarioBase=2000.0;
+		while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+			String candidato = candidatos[candidatosAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato" + candidato + " solicitou este valor de salário " + salarioPretendido);
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+				candidatosSelecionados++;
+			}
+			candidatosAtual++;
+		}
+	}
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	static void analisarCandidato(double salarioPretendido) {
 		double salarioBase = 2000.0;
